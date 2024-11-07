6a43e4aeefeda03aa2b2cbb0de08dcec1bc7a9a4
Nitiphon-Udomphot <nitphon.udo@spumail.net>
2024-11-08 02:45:32 +0700

    update

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import './Header.css';
 
-function Header({ isOpen }) {
+function Header({ isActive }) {
   return (
-    <Navbar className={`header-container ${isOpen ? 'header-shifted' : 'header-closed'}`} fixed="top" bg="light">
-      <Container fluid className="d-flex justify-content-between align-items-center header-container-content">
-        {/* Logo */}
-        <Navbar.Brand href="#" className="header-logo">
-          <img src="../public/img/metthier logo without tg-01.png" alt="Logo"/>
-        </Navbar.Brand>
-
-        {/* Username and Icon */}
-        <div className="d-flex align-items-center" align="center">
-          <span className="username">User Name <span style={{ color: 'red' }}>(Super Admin)</span></span>
-          <i className="bi bi-person-fill ms-2 person-icon" style={{ fontSize: 28 }}></i>
-        </div>
-      </Container>
-    </Navbar>
+    <div className={`header-container ${isActive ? 'header-shifted' : ''}`}>
+      <Navbar.Brand href="#dashboard"><img src="../../public/img/metthier logo without tg-01.png" alt="Metthier Logo" style={{ height: '50px' }} />
+      </Navbar.Brand>
+    </div>
   );
 }
 
 export default Header;
-
--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,78 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-function Sidebar({ tab, setTab, isOpen, setIsOpen }) {
-  const [selectedTab, setSelectedTab] = useState('');
+function Sidebar({ tab, setTab, isActive, setIsActive }) {
   const location = useLocation();
 
   useEffect(() => {
+    // Update selected tab based on current route
     switch (location.pathname) {
       case '/dashboard':
-        setSelectedTab('dashboard');
+        setTab('dashboard');
         break;
       case '/inbound':
-        setSelectedTab('inbound');
+        setTab('inbound');
         break;
       case '/outbound':
-        setSelectedTab('outbound');
+        setTab('outbound');
         break;
       case '/productlist':
-        setSelectedTab('productlist');
+        setTab('productlist');
         break;
       case '/withdraw':
-        setSelectedTab('withdraw');
+        setTab('withdraw');
         break;
       default:
-        setSelectedTab('');
+        setTab('');
     }
-  }, [location]);
+  }, [location, setTab]);
 
   const toggleSidebar = () => {
-    setIsOpen(!isOpen);
+    setIsActive(!isActive);
   };
 
   return (
-    <div className={`sidebar ${isOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
-      <Button
-        variant="primary"
-        onClick={toggleSidebar}
-        className="toggle-button"
-      >
-        {isOpen ? <i className="bi bi-chevron-double-left"></i> : <i className="bi bi-chevron-double-right"></i>}
-      </Button>
-
-      <div className="sidebar-menu">
-        <Link to="/dashboard" className={`sidebar-item ${selectedTab === 'dashboard' ? 'active' : ''}`}>
-          {isOpen && <i className="bi bi-bar-chart-line-fill"></i>}
-          {isOpen && <span>Dashboard</span>}
-          {!isOpen && <i className="bi bi-bar-chart-line-fill"></i>}
-        </Link>
-
-        <Link to="/inbound" className={`sidebar-item ${selectedTab === 'inbound' ? 'active' : ''}`}>
-          {isOpen && <i className="bi bi-arrow-down-circle-fill"></i>}
-          {isOpen && <span>Inbound</span>}
-          {!isOpen && <i className="bi bi-arrow-down-circle-fill"></i>}
-        </Link>
-
-        <Link to="/outbound" className={`sidebar-item ${selectedTab === 'outbound' ? 'active' : ''}`}>
-          {isOpen && <i className="bi bi-arrow-up-circle-fill"></i>}
-          {isOpen && <span>Outbound</span>}
-          {!isOpen && <i className="bi bi-arrow-up-circle-fill"></i>}
-        </Link>
-
-        <Link to="/productlist" className={`sidebar-item ${selectedTab === 'productlist' ? 'active' : ''}`}>
-          {isOpen && <i className="bi bi-list-ul"></i>}
-          {isOpen && <span>Product List</span>}
-          {!isOpen && <i className="bi bi-list-ul"></i>}
-        </Link>
-
-        <Link to="/withdraw" className={`sidebar-item ${selectedTab === 'withdraw' ? 'active' : ''}`}>
-          {isOpen && <i className="bi bi-box-arrow-down"></i>}
-          {isOpen && <span>Withdraw</span>}
-          {!isOpen && <i className="bi bi-box-arrow-down"></i>}
-        </Link>
+    <div className={`sidebar ${isActive ? 'active' : ''}`}>
+      <div className="logo_content">
+        <div className="logo">
+          <img src="../../public/img/metthier logo without tg-01.png" alt="Metthier Logo" style={{ height: '50px' }} />
+          <div className="logo_name">Metthier</div>
+        </div>
+        <i className="bi bi-list" id="btn" onClick={toggleSidebar}></i>
+      </div>
+      <ul className="nav_list">
+        <li className={tab === 'dashboard' ? 'active' : ''}>
+          <Link to="/dashboard">
+            <i className="bi bi-bar-chart-line-fill"></i>
+            <span className="link_name">Dashboard</span>
+          </Link>
+          <span className="tooltip">Dashboard</span>
+        </li>
+        <li className={tab === 'inbound' ? 'active' : ''}>
+          <Link to="/inbound">
+            <i className="bi bi-box-seam-fill"></i>
+            <span className="link_name">Inbound</span>
+          </Link>
+          <span className="tooltip">Inbound</span>
+        </li>
+        <li className={tab === 'outbound' ? 'active' : ''}>
+          <Link to="/outbound">
+            <i className="bi bi-box-arrow-up"></i>
+            <span className="link_name">Outbound</span>
+          </Link>
+          <span className="tooltip">Outbound</span>
+        </li>
+        <li className={tab === 'productlist' ? 'active' : ''}>
+          <Link to="/productlist">
+            <i className="bi bi-file-earmark-spreadsheet-fill"></i>
+            <span className="link_name">Log</span>
+          </Link>
+          <span className="tooltip">Log</span>
+        </li>
+        <li className={tab === 'withdraw' ? 'active' : ''}>
+          <Link to="/withdraw">
+            <i className="bi bi-person-fill"></i>
+            <span className="link_name">Personnel Information</span>
+          </Link>
+          <span className="tooltip">Personnel Information</span>
+        </li>
+        <li className={tab === 'settings' ? 'active' : ''}>
+          <Link to="/settings">
+            <i className="bi bi-gear-fill"></i>
+            <span className="link_name">Setting</span>
+          </Link>
+          <span className="tooltip">Setting</span>
+        </li>
+      </ul>
+      <div className="profile_content">
+        <div className="profile">
+          <div className="profile_details">
+            <img src="public/img/OIP (1).jpg" alt="Profile" />
+            <div className="name_job">
+              <div className="name">Nitiphon</div>
+              <div className="job">Web Developer</div>
+            </div>
+          </div>
+          <i className="bi bi-door-closed" id="log_out"></i>
+        </div>
       </div>
     </div>
   );
