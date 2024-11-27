63541a9ca8485932852244f543baff505845d5ca
Nitiphon-Udomphot <nitphon.udo@spumail.net>
2024-11-28 02:49:52 +0700

    Update Personels.jsx

--- src/Data/Personels.jsx
+++ src/Data/Personels.jsx
@@ -39,11 +39,9 @@ const Personels = [
   { 
     id: 5,
     name: "Methaphon Thongnak",
-    pass: " ",
     role: "admin",
     completed: true,
     tel: "0856789012",
-    token: "name",
   },
 ];
 
